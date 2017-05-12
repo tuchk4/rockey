a9c2dae6217acc22a093c193916a114f920d16c4
Valeriy Sorokobatko <valeriy.sorokobatko@gmail.com>
2017-05-12 16:32:55 +0300

    update clean task

--- tasks/clean.js
+++ tasks/clean.js
@@ -5,6 +5,7 @@ const chalk = require('chalk');
 
 const rockey = require('../packages/rockey/package.json');
 const rockeyRecat = require('../packages/rockey-react/package.json');
+const rockeyCssParse = require('../packages/rockey-css-parse/package.json');
 
 console.log('');
 
@@ -27,6 +28,21 @@ console.log(chalk.green('rockey-react:'));
 rockeyRecat.files.forEach(file => {
   const filePath = path.resolve('packages', 'rockey-react', file);
   const printPath = `packages/rockey-react/${file}`;
+
+  if (!fs.existsSync(filePath)) {
+    console.log(`  - ${chalk.red(printPath)}`);
+  } else {
+    console.log(`  - ${printPath}`);
+  }
+
+  rimraf.sync(filePath);
+});
+console.log('');
+
+console.log(chalk.green('rockey-css-parse:'));
+rockeyCssParse.files.forEach(file => {
+  const filePath = path.resolve('packages', 'rockey-css-parse', file);
+  const printPath = `packages/rockey-css-parse/${file}`;
 
   if (!fs.existsSync(filePath)) {
     console.log(`  - ${chalk.red(printPath)}`);
